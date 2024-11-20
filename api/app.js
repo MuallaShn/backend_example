a5762de638b671a06986ff96f724889c87a34c12
Mualla Şahin <muallashn11@gmail.com>
2024-11-20 23:07:24 +0300

    first commit

--- api/app.js
+++ api/app.js
@@ -19,6 +19,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  console.log("ben app.js te tanımlanan bir middleware'im")
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
