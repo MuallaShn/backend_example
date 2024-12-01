374c79f8cdb1ed86bbd3115b9ff47317716a6063
Mualla Şahin <muallashn11@gmail.com>
2024-12-01 21:17:41 +0300

    sistem loglari

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -5,6 +5,7 @@ const Response= require("../lib/Response");
 const CustomError =require("../lib/Error")
 const Enum = require("../config/Enum")
 const AuditLogs = require("../lib/AuditLogs");
+const logger = require("../lib/logger/LoggerClass");
 /**
 CRUD:
 *Create
@@ -49,10 +50,12 @@ router.post("/add", async (req,res) => {
         await category.save();
 
         AuditLogs.info(req.user?.email, "Categories", "Add", category);
+        logger.info(req.user?.email, "Categories", "Add",category)
 
         res.json(Response.successResponse({success:true}));
 
     }catch(err){
+        logger.error(req.user?.email, "Categories", "Add",err)
         let errorResponse =Response.errorResponse(err);
         res.status(errorResponse.code).json(errorResponse);
 
