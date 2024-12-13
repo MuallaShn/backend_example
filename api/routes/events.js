ce1d0e9fe678d8a9b7c7fc98e53dba74014e763a
Mualla Şahin <muallashn11@gmail.com>
2024-12-13 22:19:20 +0300

    emmitter

---
+++ api/routes/events.js
@@ -0,0 +1,30 @@
+const express =require("express");
+const { HTTP_CODES } = require("../config/Enum");
+const router = express.Router();
+const emitter = require("../lib/Emitter");
+
+emitter.addEmitter("notifications");
+
+
+router.get("/", async (req,res) => {
+
+    
+        res.writeHead(HTTP_CODES.OK, {
+            "content-type": "text/event-stream",
+            "connection": "keep-alive",
+            "cache-control":"no-cache , no-transform"
+        });
+
+        const listener = (data) => {
+            res.write("data:" + JSON.stringify(data) + "\n\n");
+        }
+
+        emitter.getEmitter("notifications").on("messages", listener);
+
+        req.on("close", () => {
+            emitter.getEmitter("notifications").off("messages", listener);
+        })
+ 
+});
+
+module.exports=router;
